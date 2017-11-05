91331ff8e069ea9810ca9862a413a085f44eb969
xtzhang <xtzhang@s-an.org>
2017-11-05 21:26:17 +0800

    add Interface

--- fabric_cloud_client.py
+++ fabric_cloud_client.py
@@ -13,23 +13,32 @@ def judgment(username, password):
     return flag
 
 ## 发送转账信息
-def fabric_local(buyer, seller, password):
-    flag = judgment(buyer, password) ## 身份验证
-    if flag == True:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        ip_port = ('119.29.241.15', 2017)
-        s.connect(ip_port)
-        s_data=[0]*2
-        s_data[0]=buyer
-        s_data[1]=seller
-        json_data=json.dumps(s_data)
-        s.send(json_data.encode())
-        r_data = s.recv(1024).decode()
-        s.close()
-    else:
-        r_data = "The id or password isn't correct.Please try again."
+def fabric_local(function,args):
+    # flag = judgment(buyer, password) ## 身份验证
+    # if 1 == True:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    ip_port = ('10.175.152.8', 2017)
+    s.connect(ip_port)
+    s_data=[]
+    s_data.append(function)
+    for item in args:
+        s_data.append(item)
+    json_data=json.dumps(s_data)
+    s.send(json_data.encode())
+    r_data = s.recv(1024).decode()
+    s.close()
+    # else:
+    #     r_data = "The id or password isn't correct.Please try again."
     return r_data
 
 if __name__ == "__main__":
-    result=fabric_local('person2', 'person1', 'person2')
-    print(result)
+    # print(fabric_local("addAPI",["2017.11.05.21:15","zxt","xsw","snesor0"]))
+    # print(fabric_local("addAPI",["2017.11.05.21:45","zyd","lbq","snesor1"]))
+    # print(fabric_local("addAPI",["2017.11.05.22:15","ljo","hxw","snesor2"]))
+    # print(fabric_local("addAPI",["2017.11.06.21:15","hzh","sf","snesor3"]))
+    # print(fabric_local("addAPI",["2017.12.05.21:15","dxx","ybw","snesor4"]))
+    # print(fabric_local("addAPI",["2018.12.05.21:15","xy","sch","snesor5"]))
+    print(fabric_local("get_api_group",["2017.11.05.21:15","2018.12.06"]))
+    # print(fabric_local("createUser",["c",17]))
+    # print(fabric_local("invoke",["a","b",5]))
+    # print(fabric_local("get_money",["c"]))
--- fabric_cloud_server.py
+++ fabric_cloud_server.py
@@ -3,68 +3,72 @@ import os
 import re
 import time
 import json
+import subprocess
 
-## 执行命令.并将执行结果写入文本
+# 执行命令.并将执行结果写入文本
 def execCmd(cmd):
-    r = os.popen(cmd)
-    text = r.read()
-    r.close()
+    text=subprocess.getoutput(cmd)
     return text
 
-def get_value(x):
+# 获取账户余额
+def get_money(x):
     cmd = "peer chaincode query -C $CHANNEL_NAME -n mycc -c '{\"Args\":[\"query\",\"%s\"]}'"%x
     result = execCmd(cmd)
     pat1 = ".*Query Result:.*"
     result = re.findall(pat1, result)[0]    # 找到"Query Result:"
-    m=re.search("Query Result: ",result)    # 匹配字段
+    m=re.search("Query Result: ",result)
     result=result[:m.start()]+result[m.end():] # 删除指定字符窜
     return int(result)
 
-## 获取单个api调用记录
+# 添加API
+def addAPI(s1,s2,s3,s4):
+    os.system('peer chaincode invoke -o orderer.example.com:7050  --tls $CORE_PEER_TLS_ENABLED --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem  -C $CHANNEL_NAME -n mycc -c \'{"Args":["addAPI","%s","%s","%s","%s"]}\'' % (s1,s2,s3,s4))
+    time.sleep(3)
+
+# 获取单个api调用记录
 def get_api_element(x):
     cmd = "peer chaincode query -C $CHANNEL_NAME -n mycc -c '{\"Args\":[\"query\",\"%s\"]}'" % x
     content = execCmd(cmd)
-    pattern = re.compile(r'Query Result:\s([\b\w*\b]+)_([\b\w*\b]+)_(\d+\.\d+\.\d+\.\d+:\d+)_(\d+)')
-    result = re.findall(pattern, content)[0]  # 找到"Query Result:"
-    return result[0],result[1],result[2],result[3] # 依次返回元素值
-
-## 更新api调用次数
-def update_api_times(x,y):
-    id=x+"_"+y
-    os.system('peer chaincode invoke -o orderer.example.com:7050  --tls $CORE_PEER_TLS_ENABLED --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem  -C $CHANNEL_NAME -n mycc -c \'{"Args":["updateAPItimes","%s","%s"]}\'' % (x,y))
-    time.sleep(3)
-
-## 添加新权限
-def add_authority(x,rank):
-    id="auth_"+x
-    os.system('peer chaincode invoke -o orderer.example.com:7050  --tls $CORE_PEER_TLS_ENABLED --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem  -C $CHANNEL_NAME -n mycc -c \'{"Args":["create","%s","%d"]}\'' % (id,rank))
-    time.sleep(3)
-
-## 读取权限
-def get_authority(x):
-    id = "auth_" + x
-    return get_value(id)
+    pat1 = ".*Query Result:.*"
+    result = re.findall(pat1, content)[0]  # 找到"Query Result:"
+    m = re.search("Query Result: ", result)
+    result = result[:m.start()] + result[m.end():]  # 删除指定字符窜
+    return result
 
 # 获取多个api调用记录
 def get_api_group(x,y):
     cmd = "peer chaincode invoke -o orderer.example.com:7050  --tls $CORE_PEER_TLS_ENABLED --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem  -C $CHANNEL_NAME -n mycc -c '{\"Args\":[\"testRangeQuery\",\"%s\",\"%s\"]}'" % (x,y)
     content = execCmd(cmd)
-    # pattern = ".*result: status:200 payload:.*"
-    # result = re.findall(pattern, content)[0]  # 找到result字段
-    # m = re.search("result: status:200 payload:", result)
-    # result = result[:m.start()] + result[m.end():] # 删除指定字段
-    # return result
-    return content
+    pat1 = "Chaincode invoke successful. result: status:200 payload:.*"
+    result = re.findall(pat1, content)[0]
+    m = re.search("Chaincode invoke successful. result: status:200 payload:", result)
+    result = result[:m.start()] + result[m.end():]  # 删除指定字符窜
+    pat2=","
+    result=re.split(pat2,result)
+    return result
 
-## 转账函数,self为账户自身,target为转账目标,x为转账金额
+# 转账函数,self为账户自身,target为转账目标,x为转账金额
 def invoke(self,target,x):
     os.system('peer chaincode invoke -o orderer.example.com:7050  --tls $CORE_PEER_TLS_ENABLED --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem  -C $CHANNEL_NAME -n mycc -c \'{"Args":["invoke","%s","%s","%d"]}\'' % (self,target,x))
     time.sleep(3) ## sleep3秒,等待交易确认
 
-## 创建新用户
-def create(target,x):
+# 创建新用户
+def createUser(target,x):
     os.system('peer chaincode invoke -o orderer.example.com:7050  --tls $CORE_PEER_TLS_ENABLED --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem  -C $CHANNEL_NAME -n mycc -c \'{"Args":["create","%s","%d"]}\'' % (target,x))
     time.sleep(3)
+# def update_api_times(x,y):
+#     id=x+"_"+y
+#     os.system('peer chaincode invoke -o orderer.example.com:7050  --tls $CORE_PEER_TLS_ENABLED --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem  -C $CHANNEL_NAME -n mycc -c \'{"Args":["updateAPItimes","%s","%s"]}\'' % (x,y))
+#     time.sleep(3)
+#
+# def add_authority(x,rank):
+#     id="auth_"+x
+#     os.system('peer chaincode invoke -o orderer.example.com:7050  --tls $CORE_PEER_TLS_ENABLED --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem  -C $CHANNEL_NAME -n mycc -c \'{"Args":["create","%s","%d"]}\'' % (id,rank))
+#     time.sleep(3)
+#
+# def get_authority(x):
+#     id = "auth_" + x
+#     return get_value(id)
 
 def fabric_cloud():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # 创建socket对象，用IPv4通信，协议为TCP(family, type)
@@ -75,9 +79,22 @@ def fabric_cloud():
         c, addr = s.accept()  # c为新的socket对象，addr为客户的internet地址
         r_data = c.recv(1024).decode()
         r_data = json.loads(r_data)
-        invoke(r_data[0], r_data[1], 1) ## 默认转账1
-        value=get_value(r_data[0]) ## 得到自身账户value
-        c.send(str(value).encode())
+        if r_data[0]=="get_money":
+            result=get_money(r_data[1])
+        elif r_data[0]=="get_api_element":
+            result=get_api_element(r_data[0])
+        elif r_data[0]=="get_api_group":
+            result=get_api_group(r_data[1],r_data[2])
+        elif r_data[0]=="invoke":
+            invoke(r_data[1],r_data[2],r_data[3])
+            result="The invoke operation has been done."
+        elif r_data[0]=="addAPI":
+            addAPI(r_data[1],r_data[2],r_data[3],r_data[4])
+            result="The addAPI operation has been done."
+        else:
+            createUser(r_data[1],r_data[2])
+            result="The createUser operation has been done."
+        c.send(str(result).encode())
 
 if __name__ == "__main__":
-    # fabric_cloud()
+    fabric_cloud()
